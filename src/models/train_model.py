3e72db15c43ef6a646c52f02f1885d4f0048d7ab
Ashley Alex Jacob <alexjacob260@gmail.com>
2022-12-19 16:32:15 +0500

    Added new updates

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -7,7 +7,8 @@ import numpy as np
 import pandas as pd
 from urllib.parse import urlparse
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score,recall_score,accuracy_score,precision_score,confusion_matrix,classification_report
+from sklearn.metrics import f1_score, recall_score, accuracy_score, precision_score, confusion_matrix, classification_report
+
 
 def read_params(config_path):
     """
@@ -19,53 +20,56 @@ def read_params(config_path):
         config = yaml.safe_load(yaml_file)
     return config
 
-def accuracymeasures(y_test,predictions,avg_method):
+
+def accuracymeasures(y_test, predictions, avg_method):
     accuracy = accuracy_score(y_test, predictions)
     precision = precision_score(y_test, predictions, average=avg_method)
     recall = recall_score(y_test, predictions, average=avg_method)
     f1score = f1_score(y_test, predictions, average=avg_method)
-    target_names = ['0','1']
+    target_names = ['0', '1']
     print("Classification report")
-    print("---------------------","\n")
-    print(classification_report(y_test, predictions,target_names=target_names),"\n")
+    print("---------------------", "\n")
+    print(classification_report(y_test, predictions,
+          target_names=target_names), "\n")
     print("Confusion Matrix")
-    print("---------------------","\n")
-    print(confusion_matrix(y_test, predictions),"\n")
+    print("---------------------", "\n")
+    print(confusion_matrix(y_test, predictions), "\n")
 
     print("Accuracy Measures")
-    print("---------------------","\n")
+    print("---------------------", "\n")
     print("Accuracy: ", accuracy)
     print("Precision: ", precision)
     print("Recall: ", recall)
     print("F1 Score: ", f1score)
 
-    return accuracy,precision,recall,f1score
+    return accuracy, precision, recall, f1score
 
-def get_features_and_target(df,target):
+
+def get_feat_and_target(df, target):
     """
     Get features and target variables seperately from given dataframe and target 
     input: dataframe and target column
     output: two dataframes for x and y 
     """
-    x=df.drop(target,axis=1)
-    y=df[[target]]
-    return x,y    
+    x = df.drop(target, axis=1)
+    y = df[[target]]
+    return x, y
+
 
 def train_and_evaluate(config_path):
     config = read_params(config_path)
     train_data_path = config["processed_data_config"]["train_data_csv"]
     test_data_path = config["processed_data_config"]["test_data_csv"]
     target = config["raw_data_config"]["target"]
-    max_depth=config["random_forest"]["max_depth"]
-    n_estimators=config["random_forest"]["n_estimators"]
+    max_depth = config["random_forest"]["max_depth"]
+    n_estimators = config["random_forest"]["n_estimators"]
 
     train = pd.read_csv(train_data_path, sep=",")
     test = pd.read_csv(test_data_path, sep=",")
-    train_x,train_y=get_features_and_target(train,target)
-    test_x,test_y=get_features_and_target(test,target)
+    train_x, train_y = get_feat_and_target(train, target)
+    test_x, test_y = get_feat_and_target(test, target)
 
 ################### MLFLOW ###############################
-
     mlflow_config = config["mlflow_config"]
     remote_server_uri = mlflow_config["remote_server_uri"]
 
@@ -73,35 +77,52 @@ def train_and_evaluate(config_path):
     mlflow.set_experiment(mlflow_config["experiment_name"])
 
     with mlflow.start_run(run_name=mlflow_config["run_name"]) as mlops_run:
-         model = RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators)
-         model.fit(train_x, train_y)
-         y_pred = model.predict(test_x)
-         accuracy,precision,recall,f1score = accuracymeasures(test_y,y_pred,'weighted')
-         mlflow.log_param("max_depth",max_depth)
-         mlflow.log_param("n_estimators", n_estimators)
-         mlflow.log_metric("accuracy", accuracy)
-         mlflow.log_metric("precision", precision)
-         mlflow.log_metric("recall", recall)
-         mlflow.log_metric("f1_score", f1score)
+        model = RandomForestClassifier(
+            max_depth=max_depth, n_estimators=n_estimators)
+        model.fit(train_x, train_y)
+        y_pred = model.predict(test_x)
+        accuracy, precision, recall, f1score = accuracymeasures(
+            test_y, y_pred, 'weighted')
 
-         tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+        mlflow.log_param("max_depth", max_depth)
+        mlflow.log_param("n_estimators", n_estimators)
 
-         if tracking_url_type_store != "file":
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("precision", precision)
+        mlflow.log_metric("recall", recall)
+        mlflow.log_metric("f1_score", f1score)
+        metrics_file = config["reports"]["metrics"]
+        params_file = config["reports"]["params"]
+
+        with open(metrics_file, "w") as f:
+            metrics = {
+                "accuracy": accuracy,
+                "precision": precision,
+                "recall": recall,
+                "f1_score": f1score,
+            }
+            json.dump(metrics, f, indent=4)
+
+        with open(params_file, "w") as f:
+            params = {
+                "max_depth": max_depth,
+                "n_estimators": n_estimators,
+            }
+            json.dump(params, f, indent=4)
+
+        tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+
+        if tracking_url_type_store != "file":
             mlflow.sklearn.log_model(
-                model, 
-                "model", 
+                model,
+                "model",
                 registered_model_name=mlflow_config["registered_model_name"])
-         else:
+        else:
             mlflow.sklearn.load_model(model, "model")
 
 
-    if __name__=="__main__":
-        args = argparse.ArgumentParser()
-        parsed_agrs=args.add_argument("--config",default="params.yaml")
-        parsed_agrs=args.parse_args()
-        train_and_evaluate(config_path=parsed_agrs.config)
-
-        
-
-
-
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    train_and_evaluate(config_path=parsed_args.config)
